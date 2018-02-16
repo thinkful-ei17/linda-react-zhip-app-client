c7dfc1f3b4713e33c0393fe6930bce580cd6255d
Linda Foo <linda.foo.dev@gmail.com>
2018-02-15 22:00:53 -0800

    housekeeping

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -2,17 +2,6 @@ import React from 'react';
 import {createNewUser} from '../actions/actions';
 import {reduxForm} from 'redux-form';
 import {withRouter} from "react-router-dom";
-// import React from "react";
-// import {withRouter} from "react-router-dom";
-
-// class MyComponent extends React.Component {
-//   ...
-//   myFunction() {
-//     this.props.history.push("/some/Path");
-//   }
-//   ...
-// }
-// export default withRouter(MyComponent);
 
 export class LandingPage extends React.Component{
   componentDidMount(){
@@ -20,21 +9,15 @@ export class LandingPage extends React.Component{
   }
 
   onSubmit() {
-    console.log('newUser - form component, onSubmit button was triggered, dispatch createNew User');
-    
-    const res = this.props.dispatch(createNewUser()).then(results => {
-      if(results === '1'){
+    this.props.dispatch(createNewUser()).then(results => {
+      if(results){
         this.props.history.push('/r')
       }
       return results 
     })
-
-    console.log('what is results for onSubmit', res);
-    
   }
   
   render(){
-    console.log('this is this.props', this.props);
     return (
       <div>
       <div>Hello World, You have Landed!</div>
@@ -47,16 +30,6 @@ export class LandingPage extends React.Component{
   }
 }
 
-const StupidPlaceholder = withRouter(reduxForm({form: 'newUser'})(LandingPage));
+const SmartRouter = withRouter(reduxForm({form: 'newUser'})(LandingPage));
 
-export default StupidPlaceholder
-// export default reduxForm({form: 'newUser'})(LandingPage);
-// export default withRouter(MyComponent);
-
-// class TextComponentPresentational extends React.Component {
-//   ...
-// }
-
-// const TextComponent = withResource(withSocket(withNotifications(TextComponentPresentational )))
-
-// export default TextComponent
+export default SmartRouter
--- src/components/placeholder-container.js
+++ src/components/placeholder-container.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-//import Spinner from 'react-spinkit';
-//import {initiateTransaction} from '../actions/actions';
 import Placeholder from './placeholder';
-import {REACT_APP_API_BASE_URL} from '../config';
 
 export class PlaceholderContainer extends React.Component {
     
@@ -22,26 +19,3 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps)(PlaceholderContainer);
-
-
- // renderResults() {
-    //     if (this.props.loading) {
-    //         return <Spinner spinnerName="circle" noFadeIn />;
-    //     }
-
-    //     if (this.props.error) {
-    //         return <strong>{this.props.error}</strong>;
-    //     }
-
-    //     const transaction = this.props.transactionAmount;
-    //     const transactionLink = `${REACT_APP_API_BASE_URL}/transaction/receive/${this.props.transactionId}`
-
-    //     return (
-    //         <div className="transaction-details">
-    //             <p>IOU Successfully Created!</p>
-    //             <p>IOU Amount: {transaction} </p>
-    //             <p>Send this unique link to the recipient!</p>
-    //             <p>{transactionLink}</p>
-    //         </div>
-    //     );
-    // }
