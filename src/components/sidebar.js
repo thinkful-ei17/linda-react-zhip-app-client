e599e7e0eac81e2e7b7309c0c7ab2b5e13d5d043
Linda Foo <linda.foo.dev@gmail.com>
2018-02-15 18:20:18 -0800

    create transaction is working

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import {connect} from 'react-redux';
-//import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 import '../css/sidebar.css';
 
 
 export function Sidebar(props) {
-    // const folders = props.folderList.map(folder =>
-    //     <li key={folder.id} className="folder-menu-list-item">
-    //         <Link to={`/${folder.id}`}>
-    //             {folder.name}
-    //         </Link>
-    //     </li>
-    // );
 
     return (
         <div className="sidebar sidebar-left">
-            <nav className="folder-menu">
-                <ul className="folder-menu-list">
-                    hi
+            <nav className="menu">
+                <ul className="menu-list">
+                    <li key="app-home" className="app-home menu-list-item">
+                        <Link to={`/home`}>
+                            Home
+                        </Link>
+                    </li>
+                    <li key="initiate-transaction" className="initiate-transaction menu-list-item ">
+                        <Link to={`/submit`}>
+                            Create IOU
+                        </Link>
+                    </li>
                 </ul>
             </nav>
         </div>
@@ -26,9 +28,7 @@ export function Sidebar(props) {
 }
 
 const mapStateToProps = state => ({
-    folderList: Object.keys(state).map(folderId => state[folderId])
+    
 });
 
 export default connect(mapStateToProps)(Sidebar);
-
-//                    {folders}
