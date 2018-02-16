2b340d37de3b208de500f06a9c048a2567e403f8
Linda Foo <linda.foo.dev@gmail.com>
2018-02-16 09:02:34 -0800

    minor text change

--- src/components/placeholder.js
+++ src/components/placeholder.js
@@ -9,7 +9,7 @@ export default ({accountBalance, userId}) => {
       <p className='attention-stmt'>Success!</p>
       <p><span className='key-name'>Account Balance:</span> {accountBalance}</p>
       <p><span className='key-name'>New User Id:</span> {userId}</p>
-      <p className='info-stmt'>Use your new Zhip ID to manage any IOUs you create or any IOUs you receive from other Zhip users.</p>
+      <p className='info-stmt'>Safeguard your new Zhip ID to manage any IOUs you create or any IOUs you receive from other Zhip users.</p>
       <div className='link-container'>
       <Link to={`/submit`} className='link-new-iou'>
           New IOU
